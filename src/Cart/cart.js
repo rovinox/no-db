8c0b6d9748d1b8b4e4e8863885ce9fc4ec2211e4
neaj mahmud <neajmahmud230@gmail.com>
2019-04-08 16:52:35 -0500

    updare some about page

--- src/Cart/cart.js
+++ src/Cart/cart.js
@@ -3,6 +3,7 @@ import "./Cart.css"
 import allen from "./Allen.PNG"
 import may from "./may.PNG"
 import flores from "./flores.PNG"
+import collins from "./collins.PNG"
 //this is the about page 
 
 
@@ -11,9 +12,22 @@ export default class cart extends Component {
     return (
       <div className="container">
         <p className="about">
-        You always hear car dealer dealerships telling  you  about <mark> BAD CREDIT, NO CREDIT, NO PROBLEM BULLSHIT.</mark> Here at Devmountain Auto we don’t that we only give you the car you can afford. We don’t want you to carry a loan for the rest of your life, like you’re your student loan. We have cars in all price range. Nissan, Honda, Toyota, etc. Matter the fact we even got bicycles. So come on down to South Ervay st. We’re waiting.
+        You always hear car dealer dealerships telling  you  about <mark> BAD CREDIT, NO CREDIT, NO PROBLEM BULLSHIT.</mark> Here at Devmountain Auto we don’t do that. We only give you the car you can afford. We don’t want you to carry a loan for the rest of your life, like your student loan. We have cars in all price range. Nissan, Honda, Toyota, etc. Matter the fact we even got bicycles. So come on down to South Ervay st. We’re waiting.
         </p>
+
+
+
+        
+        
+       
         <div className="managers">
+
+        <div className="self collins">
+              <h3 className="sell">Sales Manager</h3>
+              <p className="name">Collins Evulukwu</p>
+            <img className="ins" src={collins} alt="allen"></img>
+
+            </div>
 
             <div className="self">
               <h3 className="sell">General Manager</h3>
